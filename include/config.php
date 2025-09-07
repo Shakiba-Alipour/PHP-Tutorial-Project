3428bf6d450f113f13d352859bed0b8b7f192591
Shakiba Alipour <alipour.sh79@gmail.com>
2025-09-07 13:09:05 +0330

    database configurations

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define("DNS","mysql:host=localhost;dbname=tutorialDB;charset=utf8");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
---
+++ include/db.php
@@ -0,0 +1,5 @@
+<?php
+
+include("./config.php");
+
+$db = new PDO(DNS,DB_USERNAME,DB_PASSWORD);
