f2f8ad1e237db9cadcf3aa0b4b583efa7ff8ba4f
Shakiba Alipour <alipour.sh79@gmail.com>
2025-09-18 18:49:53 +0330

    feat(display posts): get all posts from DB

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 include("./include/header.php");
 include("./include/sliderphp");
 
+if (isset($_GET['category'])) {
+  $category_id = $_GET['category'];
+
+  $posts = $db->prepare('SELECT * FROM posts WHERE category_id = :id');
+  $posts->execute(['id' => $category_id]);
+ 
+} else {
+  $query_posts = "SELECT * FROM posts";
+  $posts = $db->query($query_posts);
+}
+
 ?>
 
 <?php include("./include/footer.php") ?>
